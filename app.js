b8e0af71f07b6609d83b8d7053e2b260d33b5380
KadGaurav <gauravkad139@gmail.com>
2022-09-01 18:55:21 +0530

    IMP External Authentication - Gmail

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+// https://www.passportjs.org/packages/passport-google-oauth20/  ---> refer doc for gmail Authentication 
+
 require("dotenv").config();  //For using .env(environment Varialbe) 
 const express = require("express");
 const ejs = require("ejs");
@@ -7,11 +9,11 @@ const mongoose = require("mongoose");
 // const md5 = require("md5");                      //for  hashing password
 // const bcrypt = require("bcrypt");     -->bcrypt
 // const saltRounds = 10;               -->bcrypt
-
 const session = require("express-session");        //session and cookiew automatically do salting and hasing of password
 const passport = require("passport");           // No need to declare as passport local mongoose will use it 
 const passportLocalMongoose =require("passport-local-mongoose");
-const e = require("express");
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const findOrCreate = require("mongoose-findorcreate");
 
 const app =express();
 
@@ -33,9 +35,12 @@ mongoose.connect("mongodb://localhost:27017/userDB");
 
 const userSchema = new mongoose.Schema({
     email:String,
-    password:String
+    password:String,
+    googleId:String,
+    secret:String
 });
 userSchema.plugin(passportLocalMongoose);
+userSchema.plugin(findOrCreate);
 
 //This is Encryption KEY is takken from .env file
 // userSchema.plugin(encrypt,{secret:process.env.SECRET_KEY , encryptedFields:["password"] });      --> for encrpting password
@@ -44,13 +49,46 @@ userSchema.plugin(passportLocalMongoose);
 const User = new mongoose.model("User",userSchema);
 
 passport.use(User.createStrategy());
-passport.serializeUser(User.serializeUser());
-passport.deserializeUser(User.deserializeUser());
 
+
+passport.serializeUser(function(user, done) {
+    done(null, user.id); 
+});
+passport.deserializeUser(function(id, done) {
+    User.findById(id, function(err, user) {
+        done(err, user);
+    });
+});
+
+passport.use(new GoogleStrategy({
+    clientID: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRETS,
+    callbackURL: "http://localhost:3000/auth/google/secrets",
+    userProfileURL:"https://www.googleapis.com/oauth2/v3/userinfo"
+  },
+  function(accessToken, refreshToken, profile, cb) {
+    User.findOrCreate({ googleId: profile.id }, function (err, user) {
+        console.log(profile);
+      return cb(err, user);
+    });
+  }
+))
 
 app.get("/",function(req,res){
     res.render("home");
 });
+
+app.get("/auth/google",
+  passport.authenticate('google', { scope: ["profile"] }
+  ));
+
+  app.get("/auth/google/secrets", 
+  passport.authenticate('google', { failureRedirect: "/login" }),
+  function(req, res) {
+    // Successful authentication, redirect home.
+    res.redirect('/secrets');
+  });
+
 app.get("/login",function(req,res){
     res.render("login");
 });
@@ -58,11 +96,15 @@ app.get("/register",function(req,res){
     res.render("register");
 });
 app.get("/secrets",function(req,res){
-    if(req.isAuthenticated){
-        res.render("secrets");
-    }else{
-        res.redirect("/login");
-    }
+    User.find({"secret": {$ne:null}},function(err,foundUsers){
+        if(err){
+            console.log(err);
+        }else{
+            if(foundUsers){
+                res.render("secrets", {usersWithSecrets : foundUsers});
+            }
+        }
+    }); 
 });
 app.get("/logout", function(req,res){
     req.logout(function(err) {
@@ -70,9 +112,34 @@ app.get("/logout", function(req,res){
             console.log(err);
         }
         res.redirect('/');
-      });
+    });
 });
 
+app.get("/submit",function(req,res){
+    if(req.isAuthenticated){
+        res.render("submit");
+    }else{
+        res.redirect("/login");
+    }
+    User.findById()
+    
+});
+app.post("/submit",function(req,res){
+    const submittedSecret = req.body.secret;
+
+    User.findById(req.user.id,function(err,foundUser){
+        if(err){
+            console.log(err);
+        }else{
+            if(foundUser){
+                foundUser.secret = submittedSecret;
+                foundUser.save(function(){
+                    res.redirect("/secrets");
+                });
+            }
+        }
+    })
+})
 
 //for bcrypting and salting ===>>
 
